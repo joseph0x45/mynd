21342ca7c1b8f0196fa0dfb2aa7a6b9cedaffe3c
TheWisePigeon <zozozozeph@gmail.com>
2023-05-09 23:04:11 +0000

    fix: pass token as param

--- cli/mynd.sh
+++ cli/mynd.sh
@@ -14,12 +14,11 @@ function mynd(){
 	fi
 
 	if [ "$1" = "build" ]; then
-		if [ $# -eq 2  ]; then
-			gh_token=$(<gh_token.txt)
-			docker build --build-arg GH_TOKEN=$gh_token -t $2 . 
+		if [ $# -eq 3  ]; then
+			docker build --build-arg GH_TOKEN=$3 -t $2 . 
 			return
 		else
-			echo "The build action expects 1 arguments" 
+			echo "The build action expects 2 arguments" 
 			return
 		fi
 	fi
