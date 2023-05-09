aa7ec0ca8ab6bf82fb3c39c58e32728dd3e46ce2
TheWisePigeon <zozozozeph@gmail.com>
2023-05-09 23:29:30 +0000

    fix: docker command

--- cli/mynd.sh
+++ cli/mynd.sh
@@ -26,7 +26,7 @@ function mynd(){
 	if [ "$1" = "new" ]; then
 		if [ "$2" = "kitty" ]; then
 			docker volume create $3
-			docker run  -v $3 -p 5173:5173 -dit --name $3 kitty
+			docker run  -v $3:/data -p 5173:5173 -dit --name $3 kitty
 		fi
 	fi
 
