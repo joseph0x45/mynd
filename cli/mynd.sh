20d55622b48809e095c0da61ade3b47020ff4809
TheWisePigeon <zozozozeph@gmail.com>
2023-05-09 11:35:14 +0000

    e-feat: back to pwd after update

--- cli/mynd.sh
+++ cli/mynd.sh
@@ -2,12 +2,14 @@
 
 function mynd(){
 	if [ "$1" = "update" ]; then
+		current_dir=$PWD
 		cd "$HOME/mynd"
 		git pull
+		cd "$current_dir"
 	fi
 
 	if [ "$1" = "build" ]; then
-		if [ $# -eq 3  ]; then
+		if [ $# -eq 2  ]; then
 			gh_token=$(<gh_token.txt)
 			docker build --build-arg GH_TOKEN=$gh_token -t $2 . 
 			return
