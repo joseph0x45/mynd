a720714c00e7768e9f0641501dd48d4d9ae70a2a
TheWisePigeon <zozozozeph@gmail.com>
2023-05-09 23:17:32 +0000

    feat: run container

--- cli/mynd.sh
+++ cli/mynd.sh
@@ -23,6 +23,13 @@ function mynd(){
 		fi
 	fi
 
+	if [ "$1" = "new" ]; then
+		if [ "$2" = "kitty" ]; then
+			docker volume create $3
+			docker run  -v $3 -p 5173:5173 -dit --name $3 kitty
+		fi
+	fi
+
 }
 
 
