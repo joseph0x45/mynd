920895f7ed885b670badb083f9436e0f833bb300
TheWisePigeon <zozozozeph@gmail.com>
2023-05-10 18:29:32 +0000

    feat: add deno image

--- cli/mynd.sh
+++ cli/mynd.sh
@@ -14,11 +14,11 @@ function mynd(){
 	fi
 
 	if [ "$1" = "build" ]; then
-		if [ $# -eq 3  ]; then
-			docker build --build-arg gh_token=$3 -t $2 . 
+		if [ $# -eq 2  ]; then
+			docker build -t $2 . 
 			return
 		else
-			echo "The build action expects 2 arguments" 
+			echo "The build action expects 1 argument" 
 			return
 		fi
 	fi
