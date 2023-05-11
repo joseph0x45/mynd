df301fdd01d7bbd9cb39059395671a4bcd89ac97
TheWisePigeon <zozozozeph@gmail.com>
2023-05-11 07:15:06 +0000

    feat: add port binding and error handling

--- cli/mynd.sh
+++ cli/mynd.sh
@@ -25,7 +25,19 @@ function mynd(){
 
 	if [ "$1" = "new" ]; then
 		if [ "$2" = "kitty" ]; then
-			docker run  -v $3:/data -p 5173:5173 -dit --name $3 kitty
+            if [[ $# -ge 4 ]]; then
+                port_binding="-p $4"
+            fi
+            echo "🔃 Spinning up container and volume"
+            container_id = $(docker run $port_binding -v $3:/data -dit --name $3 kitty)
+            if [ $? -ne 0 ]; then
+                echo "❌ Failed to create container"
+                echo "$container_id"
+                return
+            fi
+            echo "✅ Container ready"
+            echo "🔗 Connecting to container"
+            docker exec -it $container_id  bash
 		fi
 	fi
 
